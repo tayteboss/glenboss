c6f4960ffcb9210d534de7d1f18b4609d9a6968d
Tayte Boss <boss.tayte@gmail.com>
2023-04-17 10:14:13 +1000

    [chore] Disallow google crawling on staging

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,8 @@
-const robotsPolicy = [{ userAgent: '*', allow: '/' }];
+let robotsPolicy = [{ userAgent: '*', allow: '/' }];
+if( process.env.ENVIRONMENT === 'staging' )
+{
+	robotsPolicy = [{ userAgent: '*', disallow: '/' }];
+}
 
 module.exports = {
 	siteUrl: process.env.SITE_URL || 'https://localhost:3000',
@@ -6,4 +10,4 @@ module.exports = {
 	robotsTxtOptions: {
 		policies: robotsPolicy,
 	},
-};
+}
